11418820f3e31910d9846d7ef12ab04af998ec90
Rob Durst <me@robdurst.com>
2024-11-30 08:42:34 -0700

    use hosted model or fall back to local one

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template
-from herpWizard.model import perform_inference
+from herpWizard.model import fetch_model_name, perform_inference
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def index():
 
     if request.method == "POST":
         result = perform_inference(request.form.get("text_input"))
-    return render_template("home.html", result=result)
+    return render_template("home.html", result=result, model_name=fetch_model_name())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=False)
--- herpWizard/model.py
+++ herpWizard/model.py
@@ -1,2 +1,29 @@
+import requests
+
 def perform_inference(input_data):
-    return "42 is the answer to life, the universe, and everything."
+    ai_url = "http://localhost:11434/api/chat"
+    payload = {
+        "model": "llama3.2",
+        "messages": [
+            {
+                "role": "user",
+                "content": input_data
+            }
+        ],
+        "stream": False
+    }
+    
+    try:
+        return requests.post(ai_url, json=payload, timeout=5).json()["message"]["content"]
+    except requests.exceptions.RequestException as e:
+        print(f"Error contacting AI model: {e}")
+        return "42 is the answer to life, the universe, and everything."
+
+def fetch_model_name():
+    ai_url = "http://localhost:11434/api/model"
+    
+    try:
+        return requests.get(ai_url, timeout=5).json()["model_name"]
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching model name: {e}")
+        return "herp-wizard-0.1"
